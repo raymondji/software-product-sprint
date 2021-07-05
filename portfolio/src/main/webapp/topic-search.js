66a44e1f9f0ed24ddca755efd47d683fc3681cf0
jamari-morrison <RedCater360@yahoo.com>
2021-07-05 22:43:04 +0000

    load topic data from server and dynamically set html

--- portfolio/src/main/webapp/topic-search.js
+++ portfolio/src/main/webapp/topic-search.js
@@ -1,7 +1,23 @@
-async function getTopics(){const responseFromServer = await fetch('/get-topics');
+async function loadTopics(){const responseFromServer = await fetch('/get-topics');
 const topicData = await responseFromServer.json();
-console.log(topicData)
+
+//maybe have this randomly select from the set without replacement?
+const recommendedTopics = [0, 1, 2]
+setTopicData(recommendedTopics, 1, topicData);
+setTopicData(recommendedTopics, 2, topicData);
+setTopicData(recommendedTopics, 3, topicData);
+
+}
+
+function setTopicData(recommendedTopics, topicNum, topicData){
+var topicTitle = document.getElementById('topic-'+topicNum+'-title');
+var topicImg = document.getElementById('topic-'+topicNum+'-img');
+var topicDesc = document.getElementById('topic-'+topicNum+'-desc');
+
+topicTitle.innerHTML=topicData.topics[recommendedTopics[topicNum-1]].title
+topicDesc.innerHTML=topicData.topics[recommendedTopics[topicNum-1]].desc
+topicImg.src= 'images/'+topicData.topics[recommendedTopics[topicNum-1]].id+'.png'
 }
 
 
-getTopics()
+loadTopics()
