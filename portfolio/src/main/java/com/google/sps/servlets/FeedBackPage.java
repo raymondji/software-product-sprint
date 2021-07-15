e7f9011201ceffd8c44f90e9a94356cb95ff1aed
Marlon <marlon.romolpz@gmail.com>
2021-07-15 21:55:47 +0000

    Complete the datastore saving and put different names for .html, .css and .js

--- portfolio/src/main/java/com/google/sps/servlets/FeedBackPage.java
+++ portfolio/src/main/java/com/google/sps/servlets/FeedBackPage.java
@@ -1,10 +1,10 @@
 package com.google.sps.servlets;
 
-/*import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.Datastore;
 import com.google.cloud.datastore.DatastoreOptions;
 import com.google.cloud.datastore.Entity;
 import com.google.cloud.datastore.FullEntity;
-import com.google.cloud.datastore.KeyFactory;*/
+import com.google.cloud.datastore.KeyFactory;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,8 +21,8 @@ public final class FeedBackPage extends HttpServlet {
         final String name = request.getParameter("name-input");
         final String helpful = request.getParameter("helpful-input");
 
-       // Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
-       // KeyFactory keyFactory = datastore.newKeyFactory().setKind("FeedBack");
+        Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+        KeyFactory keyFactory = datastore.newKeyFactory().setKind("FeedBack");
         
         if (!name.equals("") && !helpful.equals("")) {
             System.out.println("You submitted this data: ");
@@ -33,9 +33,9 @@ public final class FeedBackPage extends HttpServlet {
             response.getWriter().println("Name: " + name);
             response.getWriter().println("Helpful?: " + helpful);
 
-         //   FullEntity feedBackEntity = Entity.newBuilder(keyFactory.newKey()).set("name", name).set("helpful", helpful).build();
+         FullEntity feedBackEntity = Entity.newBuilder(keyFactory.newKey()).set("name", name).set("helpful", helpful).build();
 
-           // datastore.put(feedBackEntity);
+            datastore.put(feedBackEntity);
         }
     }
 }
